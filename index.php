0d22d7d4f50950fe2d1c07ae75caa1f92e18b3bd
Chris Riebschlager <riebschlager@gmail.com>
2013-07-22 15:59:31 -0500

    Getting it going.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Art - The816</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Artwork created by WindOut, an interactive installation by Chris Riebschlager.">
+  <meta name="author" content="Chris Riebschlager">
+  <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container">
+  </div>
+  <script src="/assets/js/jquery-1.10.2.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
