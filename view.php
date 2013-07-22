8011c520c66965a7c19237ee20a83f18fb4c7232
Chris Riebschlager <riebschlager@gmail.com>
2013-07-22 16:10:49 -0500

    Ugh.

--- view.php
+++ view.php
@@ -1,5 +1,5 @@
 <?php 
 	echo "<img src=\"";
-	echo "https://dl.dropboxusercontent.com/u/3308391/windout/" . echo $_GET['id'] . ".jpg";
+	echo "https://dl.dropboxusercontent.com/u/3308391/windout/" . $_GET['id'] . ".jpg";
 	echo ">";
 ?>
