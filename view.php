7d3ead505ad2774cf476e450d8e33558e48b0930
Chris Riebschlager <riebschlager@gmail.com>
2013-07-23 18:45:33 -0500

    Getting caught up.

--- view.php
+++ view.php
@@ -1,4 +1,25 @@
-<?php 
-	$imgTag = "<img src=\"https://dl.dropboxusercontent.com/u/3308391/windout/".$_GET['id'].".jpg\">";
-	echo $imgTag;
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Art - The816</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Artwork created by WindOut, an interactive installation by Chris Riebschlager.">
+  <meta name="author" content="Chris Riebschlager">
+  <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container">
+ <?php
+ 	$imgTag = "<img src=\"https://dl.dropboxusercontent.com/u/3308391/windout/".$_GET['id'].".jpg\">";
+ 	echo $imgTag;
+ ?>
+  </div>
+  <script src="/assets/js/jquery-1.10.2.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
